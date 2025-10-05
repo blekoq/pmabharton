bdf7d45bab41a7b4265cea189bd3277af947ea2d
blekoq <taufiq.alby2014@gmail.com>
2025-10-05 16:58:05 +0700

    new repo

---
+++ drawing.py
@@ -0,0 +1,18 @@
+import time 
+from turtle import *
+
+blekoq=Turtle()
+blekoq.shape("turtle")
+blekoq.color("green")
+blekoq.pensize(3)
+blekoq.fillcolor("red")
+blekoq.begin_fill()
+for i in range(4):
+    blekoq.forward(100)
+    blekoq.right(90)
+blekoq.end_fill()
+blekoq.forward(50)
+for i in range(20)::
+    blekoq.forward(10)
+    blekoq.right(18)
+time.sleep(10)
