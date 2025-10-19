b3cf58dd056df328550d5564b5d0a39eef4077f1
blekoq <taufiq.alby2014@gmail.com>
2025-10-19 17:29:38 +0700

    cabang dan operator

---
+++ cabang.py
@@ -0,0 +1,9 @@
+kondisi=input("bagaimana kondisimu sekarang?(laper,sakit,kebelet eeq)")
+if(kondisi=="laper"):
+    print("KAU LAPER MINUM PROMAG")
+elif(kondisi=="sakit"):
+    print("mangkanya ke dokter bukan ke rumah tetangga")
+elif(kondisi=="kebelet eeq"):
+    print("kangker")
+else:
+    print("SIUH SIUH SIUH BUKAN URUSAN KU")
---
+++ operator.py
@@ -0,0 +1,12 @@
+# operator MATEMATIKA
+a = int(input("masukan a di :"))        
+b = int(input("masukan b di :"))
+print("a + b = ",a+b),
+print("a - b = ",a-b),
+print("a x b = ",a*b),
+print("a : b = ",a/b),
+# operatorlogika
+print("a = b = ",a==b),
+print("a != b = ",a!=b),
+print("a > b = ",a>b),
+print("a < b = ",a<b),
