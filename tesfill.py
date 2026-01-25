0a1525b6b7f4876e9daa5d7fbf745ba8116fdb3b
blekoq <taufiq.alby2014@gmail.com>
2026-01-25 17:27:55 +0700

    upgrade tugas dengan warna random

---
+++ tesfill.py
@@ -0,0 +1,9 @@
+from turtle import *
+import time
+pencolor("red")
+fillcolor("blue")
+width(5)
+begin_fill()
+circle(100)
+end_fill()
+time.sleep(3)
--- tugas.py
+++ tugas.py
@@ -1,17 +1,30 @@
 from turtle import *
 import time
+from random import random
 turtle=Turtle()
 screen=Screen()
 screen.screensize(600,600)
 turtle.width(5)
+turtle.speed(0)
 turtle.pencolor("cyan")
+
 turtle.shape("classic")
+def square(size):
+    for i in range(4):
+        turtle.forward(size)
+        turtle.left(90) 
 for i in range(8):
     for x in range(4):
         turtle.forward(50)
         turtle.left(75)
         turtle.forward(50)
-        turtle.square(50)
+        r=random()
+        g=random()
+        b=random()
+        turtle.fillcolor(r,g,b)
+        turtle.begin_fill()
+        square(50)
+        turtle.end_fill()
         turtle.backward(50)
         turtle.right(75)
 
@@ -19,9 +32,21 @@ for i in range(8):
         
         turtle.right(75)
         turtle.forward(50)
+        turtle.right(90)
+        r=random()
+        g=random()
+        b=random()
+        turtle.fillcolor(r,g,b)
+        turtle.begin_fill()
+        square(50)
+        turtle.end_fill()
+        turtle.left(90)
         turtle.backward(50)
+        
         turtle.left(75)
+       
         turtle.backward(50)
     turtle.left(45)
 
 time.sleep(3)
+
