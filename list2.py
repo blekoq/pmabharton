99975be069c062541c6580203256ae74025917c6
blekoq <taufiq.alby2014@gmail.com>
2026-05-17 16:36:10 +0700

    tuple

---
+++ list2.py
@@ -0,0 +1,11 @@
+untukbuahbuahan=["apple","jeruk","buah naga","semangka"]
+hargabuah=[70000,80000,300000,100000]
+print(untukbuahbuahan)
+print(untukbuahbuahan[1:3])
+untukhewan=("walang","walang sanget","lembeng","curut")
+print(untukhewan)
+print(untukhewan[1:3])
+untukbuahbuahan[0]="buah apel"
+print(untukbuahbuahan)
+untukhewan[0]="belalang"
+print(untukhewan)
