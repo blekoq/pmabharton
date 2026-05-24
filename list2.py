cbe2b2cd46ea1f17f8bceffffd11dbe2f1d76d2d
blekoq <taufiq.alby2014@gmail.com>
2026-05-24 16:30:17 +0700

    Github

--- list2.py
+++ list2.py
@@ -1,11 +1,39 @@
-untukbuahbuahan=["apple","jeruk","buah naga","semangka"]
+untukbuahbuahan=["apple","jeruk","buah naga","semangka"]#list
 hargabuah=[70000,80000,300000,100000]
-print(untukbuahbuahan)
-print(untukbuahbuahan[1:3])
-untukhewan=("walang","walang sanget","lembeng","curut")
-print(untukhewan)
-print(untukhewan[1:3])
+# print(untukbuahbuahan)
+# print(untukbuahbuahan[1:3])
+untukhewan=("walang","walang sanget","lembeng","curut")#tuple  
+# print(untukhewan[1:3])
 untukbuahbuahan[0]="buah apel"
-print(untukbuahbuahan)
-untukhewan[0]="belalang"
-print(untukhewan)
+# print(untukbuahbuahan)
+# untukhewan[0]="belalang"
+# print(untukhewan)
+untukbuahbuahan.append("buah belimbeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeng")
+# print(untukbuahbuahan)
+# untukhewan.append("aing maungggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggsggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggoggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggsgggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg")
+# print(untukhewan)
+untuksayur={"kol":7000000,"kubeis":800000,"beton":3000000,"sambel":700000}#dictionary
+# print(untuksayur)
+untuksayur["kol"]=10000000
+# print(untuksayur)
+untuksayur["sambel bledek khas kali ciliwung"]=10000000
+# print(untuksayur)
+print("welcome to pasar mundur makmur sayur dan buah")
+print("daftar sayur yang tersedia:")
+angka=1
+for sayur in untuksayur:
+    print(angka, ".", sayur, ": Rp", untuksayur[sayur])
+    angka+=1
+totalbelanja=0
+daftabelanja={}
+
+while True:
+    print("masukan angka sayur yang ingin di beli:")
+    pilihan=input()
+    jumlah=int(input("berapa kilo :"))
+    daftabelanja[pilihan]=jumlah
+    selesai=input("apakah sudah selesai belanja? (y/n)")
+    if selesai.lower() == "y":
+        break
+print("daftar belanjaan anda:")
+print(daftabelanja)
